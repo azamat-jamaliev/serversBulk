3f136b6119f3f8dc8f2f38604efc36021e17013f
azia0416 <azamat.iamaliev@netcracker.com>
2022-06-29 22:45:19 +1000

    Updated to use task management

--- serversBulk.go
+++ serversBulk.go
@@ -14,24 +14,60 @@ import (
 	"serversBulk/modules/sshHelper"
 
 	"github.com/fatih/color"
-	"golang.org/x/crypto/ssh"
 )
 
 type CurrentFileForDownloading struct {
 	Server       string
 	FileName     string
 	ConfigServer configProvider.ConfigServerType
-	SshConfig    *ssh.ClientConfig
-	Error        error
+	// SshConfig    *ssh.ClientConfig
+	Error error
+}
+
+type TaskStatus string
+
+const (
+	Planned    TaskStatus = "Planned"
+	InProgress TaskStatus = "InProgress"
+	Finished   TaskStatus = "Finished"
+	Failed     TaskStatus = "Failed"
+)
+
+type TaskType string
+
+const (
+	TypeUploadFile      TaskType = "UploadFile"
+	TypeArchiveLogs     TaskType = "ArchiveLogs"
+	TypeArchiveGzip     TaskType = "TypeArchiveGzip"
+	TypeDownloadArchive TaskType = "DownloadLogs"
+	TypeGrepInLogs      TaskType = "GrepInLogs"
+	TypeExecuteCommand  TaskType = "ExecuteCommand"
+)
+
+type ServerTask struct {
+	Type            TaskType
+	Status          TaskStatus
+	FileName        string
+	Log             string
+	JsonFileName    string
+	ServersName     string
+	ModifTime       string
+	GrepFor         string
+	ExecuteCmd      string
+	LogDir          string
+	UploadLocalFile string
+	LogFilePattern  string
+	ConfigServer    configProvider.ConfigServerType
+	Server          string
+	Error           error
 }
 
 func main() {
-	logHelper.LogPrintln("******* START serversBulk *********")
-	// fmt.Println("The tool for quick serach in the logs of different nodes\nand servers when Grafana is not available")
-	// fmt.Println("EXAMPLES:\n\tto search in logs:\n\t\t./serversBulk -s \"search\" > ./output.txt")
-	// fmt.Println("\t\t./serversBulk -c ./serversBulk_config_SVT.json -s \"18\\:.*\\[ERROR\"")
-	// fmt.Println("\n\tto download logs:\n\t\t./serversBulk -d ./logs/")
+	logHelper.LogPrintln("********************************************************************************")
+	logHelper.LogPrintln("*                           ServersBulk                                        *")
+	logHelper.LogPrintln("********************************************************************************")
 
+	var taskName TaskType
 	jsonFileName := flag.String("c", "./config/serversBulk_config.json", "path to environment configuration file")
 	serversName := flag.String("servers", "", "to search/download only from the servers with NAME='servers', \n\tfor example if you need to download from TBAPI\n\tserevers you can use parameter: `--servers TBAPI` ")
 	modifTime := flag.String("mtime", "-0.2", "same as mtime for 'find'")
@@ -41,76 +77,123 @@ func main() {
 	uploadLocalFile := flag.String("u", "", "File which will be uploaded to /var/tmp to the target servers")
 	logFilePattern := flag.String("f", "", "log File pattern: i.e. *.log the value will overwrite value in config")
 	flag.Parse()
-	if *grepFor == "" && *logDir == "" && *executeCmd == "" && *uploadLocalFile == "" {
-		logHelper.ErrFatal(errors.New("-s or -d or -e or -u should be specified"))
-	} else if *grepFor != "" && *logDir != "" {
-		logHelper.ErrFatal(errors.New("only one of these: -s or -d should be specified"))
+	switch {
+	case *grepFor != "":
+		taskName = TypeGrepInLogs
+	case *executeCmd != "":
+		taskName = TypeExecuteCommand
+	case *logDir != "":
+		taskName = TypeArchiveLogs
+	case *uploadLocalFile != "":
+		taskName = TypeUploadFile
+	default:
+		panic("Operation is not defined - please use --help ")
 	}
 
 	config := configProvider.GetConfig(jsonFileName)
-	// Calculate number of channels
-	numberOfChannels := 0
+
+	numberOfServers := 0
+	tasksChannel := make(chan ServerTask) //, numberOfServers)
 	for _, serverConf := range config.Servers {
 		if serverConf.IpAddresses != nil && (*serversName == "" || serverConf.Name == *serversName) {
-			numberOfChannels += len(serverConf.IpAddresses)
-		}
-	}
-	logHelper.LogPrintf("calculated number of channels/servers to connect=[%v]", numberOfChannels)
-	if numberOfChannels < 1 {
-		logHelper.ErrFatal(errors.New("servers to connect is less than 1 - nothing to do"))
-	}
-
-	resultChannels := make(chan string)                     //, numberOfChannels)
-	zipFilesChannel := make(chan CurrentFileForDownloading) //, numberOfChannels)
-	for _, serverConf_p := range config.Servers {
-		serverConf := serverConf_p
-		if serverConf.IpAddresses != nil && (*serversName == "" || serverConf.Name == *serversName) {
+			numberOfServers += len(serverConf.IpAddresses)
 			if *logFilePattern != "" {
 				serverConf.LogFilePattern = *logFilePattern
 			}
 			for _, serverIp := range serverConf.IpAddresses {
-				if *executeCmd != "" {
-					go executeOnServerWithOutput(&serverConf, serverIp, *executeCmd, resultChannels)
-				} else if *grepFor != "" {
-					go grepOnServer(&serverConf, serverIp, *grepFor, *modifTime, resultChannels)
-				} else if *uploadLocalFile != "" {
-					go uploadFile(&serverConf, serverIp, *uploadLocalFile, "/var/tmp", resultChannels)
-				} else {
-					go prepareZipLog(&serverConf, serverIp, *modifTime, zipFilesChannel)
-					go downloadZipLog(&serverConf, *logDir, zipFilesChannel, resultChannels)
-				}
+				go func(serverIp string, serverConf configProvider.ConfigServerType) {
+					task := ServerTask{Status: Planned,
+						Type:            taskName,
+						JsonFileName:    *jsonFileName,
+						ServersName:     *serversName,
+						ModifTime:       *modifTime,
+						GrepFor:         *grepFor,
+						ExecuteCmd:      *executeCmd,
+						LogDir:          *logDir,
+						UploadLocalFile: *uploadLocalFile,
+						LogFilePattern:  *logFilePattern,
+						ConfigServer:    serverConf,
+						Server:          serverIp}
+					tasksChannel <- task
+				}(serverIp, serverConf)
+
 			}
 		}
 	}
-
-	// logPrintln("Waiting for complition")
-	// wg.Wait()
-
-	// fmt.Printf("Result: %s \n", results)
+	logHelper.LogPrintf("calculated number of servers to connect=[%v]\n", numberOfServers)
+	if numberOfServers < 1 {
+		logHelper.ErrFatal(errors.New("servers to connect is less than 1 - nothing to do"))
+	}
 	count := 0
-	for i := range resultChannels {
-		count++
-		fmt.Println("")
-		fmt.Println("")
-		logHelper.LogPrintf("count: %v of %v", count, numberOfChannels)
-		fmt.Printf("Result: %v of %v\n", count, numberOfChannels)
-		fmt.Println("↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓")
-		fmt.Print(i)
-		fmt.Println("")
-		fmt.Println("↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑")
-		if count >= numberOfChannels {
+	for task := range tasksChannel {
+		if task.Status == Finished || task.Status == Failed {
+			count++
+			//Print finished task:
+			PrintTask(&task)
+		} else {
+			//print Inprogress task
+			switch task.Type {
+			case TypeGrepInLogs:
+				go grepInLogs(task, tasksChannel)
+			case TypeExecuteCommand:
+				go executeCommand(task, tasksChannel)
+			case TypeArchiveLogs:
+				go archiveLogs(task, tasksChannel)
+			case TypeArchiveGzip:
+				go archiveGzip(task, tasksChannel)
+			case TypeDownloadArchive:
+				go downloadZipLog(task, tasksChannel)
+			case TypeUploadFile:
+				go uploadFile(task, tasksChannel)
+			default:
+				panic(fmt.Sprintf("unknown task type task.Type=%v", task.Type))
+			}
+		}
+		if count >= numberOfServers {
 			break
 		}
 	}
 
-	//Species: pigeon, Description: likes to perch on rocks
-	logHelper.LogPrintln("FINISHED SUCCESFULLY")
+}
+func PrintTask(task *ServerTask) {
+	var clr *color.Color
+	if task.Status == Failed {
+		clr = color.New(color.FgRed)
+		clr = clr.Add(color.Bold)
+	} else if task.Status == Finished {
+		clr = color.New(color.FgGreen)
+		clr = clr.Add(color.Bold)
+	} else {
+		clr = color.New(color.BgWhite)
+	}
+	yellColor := color.New(color.FgYellow)
+	yellColor = yellColor.Add(color.Bold)
+	clr.Println("")
+	clr.Println("↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓")
+	yellColor.Printf("SERVER: %s\n", task.Server)
+	clr.Printf("NAME: %s\n", task.Type)
+	clr.Printf("STATUS: %s\n", task.Status)
+	if task.Status == Failed {
+		clr.Println(task.ExecuteCmd)
+		clr.Println(task.Error)
+	}
+	clr.Println(task.FileName)
+	clr.Println(task.Log)
+	clr.Println("↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑")
 }
 
-// NOTE: output result is in the channel
-func grepOnServer(serverConf *configProvider.ConfigServerType, server, grepFor, modifTime string, output chan<- string) {
-	cmd := fmt.Sprintf("find %s -type f -iname \"%s\" -mtime %s -exec grep --color=auto -H -A15 -B15 \"%s\" {}  \\;", serverConf.LogFolder, serverConf.LogFilePattern, modifTime, grepFor)
-	executeOnServerWithOutput(serverConf, server, cmd, output)
+func taskForChannel(task *ServerTask, log string, err error, newStatus TaskStatus, nextTask *TaskType) *ServerTask {
+	task.Log = log
+	task.Error = err
+	if err != nil {
+		task.Status = Failed
+	} else {
+		task.Status = newStatus
+		if nextTask != nil {
+			task.Type = *nextTask
+		}
+	}
+	return task
 }
 func executeOnServer(serverConf *configProvider.ConfigServerType, server, cmd string) (string, error) {
 	logHelper.LogPrintf("executeOnServer server: %s", server)
@@ -122,100 +205,88 @@ func executeOnServer(serverConf *configProvider.ConfigServerType, server, cmd st
 	if e != nil {
 		logHelper.ErrLogWinMessage(fmt.Sprintf("error while executing cmd:[%s] os server [%s], cmd_output[%s]", cmd, server, buff), e)
 	}
-	str := fmt.Sprintf("\nSERVER:%s\n_____________________________\n%s", color.HiYellowString(server), string(buff))
+	str := string(buff)
 	return str, e
 }
-func executeOnServerWithOutput(serverConf *configProvider.ConfigServerType, server, cmd string, output chan<- string) {
-	str, _ := executeOnServer(serverConf, server, cmd)
-	output <- str
+
+func executeCommand(task ServerTask, output chan<- ServerTask) {
+	str, e := executeOnServer(&task.ConfigServer, task.Server, task.ExecuteCmd)
+	output <- *taskForChannel(&task, str, e, Finished, nil)
 }
 
-func prepareZipLog(serverConf *configProvider.ConfigServerType, server, modifTime string, outZipFilename chan<- CurrentFileForDownloading) {
+// NOTE: output result is in the channel
+func grepInLogs(task ServerTask, output chan<- ServerTask) {
+	task.ExecuteCmd = fmt.Sprintf("find %s -type f -iname \"%s\" -mtime %s -exec grep --color=auto -H -A15 -B15 \"%s\" {}  \\;", task.ConfigServer.LogFolder, task.ConfigServer.LogFilePattern, task.ModifTime, task.GrepFor)
+	str, e := executeOnServer(&task.ConfigServer, task.Server, task.ExecuteCmd)
+	output <- *taskForChannel(&task, str, e, Finished, nil)
+}
+func archiveLogs(task ServerTask, output chan<- ServerTask) {
+	tarNamefile := fmt.Sprintf("%s/%s.%s", task.ConfigServer.LogFolder, strings.ReplaceAll(task.Server, ".", "_"), "tar")
+	cmd := fmt.Sprintf("cd %s; find ./ -type f -iname \"%s\" -mtime %s -exec tar rvf %s {} \\;", task.ConfigServer.LogFolder, task.ConfigServer.LogFilePattern, task.ModifTime, tarNamefile)
+	str, e := executeOnServer(&task.ConfigServer, task.Server, cmd)
 
-	tarNamefile := fmt.Sprintf("%s/%s.%s", serverConf.LogFolder, strings.ReplaceAll(server, ".", "_"), "tar")
-	cmd := fmt.Sprintf("cd %s; find ./ -type f -iname \"%s\" -mtime %s -exec tar rvf %s {} \\;", serverConf.LogFolder, serverConf.LogFilePattern, modifTime, tarNamefile)
-	_, e := executeOnServer(serverConf, server, cmd)
-	if e == nil {
-		tarGzNamefile := fmt.Sprintf("%s.gz", tarNamefile)
+	nextTask := TypeArchiveGzip
+	task.FileName = tarNamefile
+	output <- *taskForChannel(&task, str, e, InProgress, &nextTask)
+}
+func archiveGzip(task ServerTask, output chan<- ServerTask) {
+	tarGzNamefile := fmt.Sprintf("%s.gz", task.FileName)
+	task.ExecuteCmd = fmt.Sprintf("if test -f '%s'; then cd %s; tar cvzf %s %s ; rm %s;echo 'true';fi", task.FileName, path.Dir(tarGzNamefile), path.Base(tarGzNamefile), path.Base(task.FileName), task.FileName)
+	str, e := executeOnServer(&task.ConfigServer, task.Server, task.ExecuteCmd)
+	fmt.Printf("RESPONSE str [%s]\n", str)
 
-		cmdGz := fmt.Sprintf("cd %s; tar cvzf %s %s ; rm %s", path.Dir(tarGzNamefile), path.Base(tarGzNamefile), path.Base(tarNamefile), tarNamefile)
-		if _, e = executeOnServer(serverConf, server, cmdGz); e == nil {
-			outZipFilename <- CurrentFileForDownloading{FileName: tarGzNamefile, Server: server, ConfigServer: *serverConf}
-		} else {
-			outZipFilename <- CurrentFileForDownloading{Server: server, Error: e}
-		}
-	} else {
-		outZipFilename <- CurrentFileForDownloading{Server: server, Error: e}
+	nextTask := TypeDownloadArchive
+	task.FileName = tarGzNamefile
+	output <- *taskForChannel(&task, str, e, InProgress, &nextTask)
+}
+func downloadZipLog(task ServerTask, output chan<- ServerTask) {
+	localZipFileName := path.Join(task.LogDir, path.Base(task.FileName))
+	fmt.Printf("DOWNLOADING file [%s] to [%s]\n", task.FileName, localZipFileName)
+
+	sshAdv := sshHelper.OpenSshAdvanced(&task.ConfigServer, task.Server)
+	defer sshAdv.Close()
+	sftpClient := sshAdv.NewSftpClient()
+	srcFile, err := sftpClient.OpenFile(task.FileName, (os.O_RDONLY))
+	if err != nil {
+		output <- *taskForChannel(&task, "Unable to download file", err, Finished, nil)
+		return
 	}
-}
 
-func downloadZipLog(serverConf *configProvider.ConfigServerType, localDir string, inZipFilename chan CurrentFileForDownloading, output chan<- string) {
-	zipFileSrv := <-inZipFilename
-	if zipFileSrv.Error == nil {
-		zipFilePath := zipFileSrv.FileName
-		server := zipFileSrv.Server
-		zipFileName := path.Base(zipFilePath)
-		localZipFileName := path.Join(localDir, zipFileName)
-
-		logHelper.LogPrintf("Downloading File=[%s] from server=[%s] to local path=[%s] ...", zipFilePath, server, localZipFileName)
-
-		sshAdv := sshHelper.OpenSshAdvanced(serverConf, server)
-		defer sshAdv.Close()
-		sftpClient := sshAdv.NewSftpClient()
-		srcFile, err := sftpClient.OpenFile(zipFilePath, (os.O_RDONLY))
-		if err != nil {
-			logHelper.ErrFatalWithMessage(
-				fmt.Sprintf("Unable to open file=[%s] on server=[%s]", zipFilePath, server),
-				err)
-		}
-
-		dstFile, err := os.Create(localZipFileName)
-		if err != nil {
-			logHelper.ErrFatal(err)
-		}
-		defer dstFile.Close()
-
-		_, err = io.Copy(dstFile, srcFile)
-		if err != nil {
-			logHelper.ErrFatal(err)
-		}
-
-		logHelper.LogPrintf("removing file:%s\n", zipFilePath)
-		err = sftpClient.Remove(zipFilePath)
-		if err != nil {
-			logHelper.ErrFatal(err)
-		}
-
-		output <- fmt.Sprintf("Created File: %s source server: %s", localZipFileName, server)
-	} else {
-		output <- fmt.Sprintf("Server: %s finished with Error: %s", zipFileSrv.Server, zipFileSrv.Error)
+	dstFile, err := os.Create(localZipFileName)
+	if err != nil {
+		output <- *taskForChannel(&task, fmt.Sprintf("Unable to create file [%s]", localZipFileName), err, Finished, nil)
+		return
 	}
+	defer dstFile.Close()
+
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		output <- *taskForChannel(&task, fmt.Sprintf("Unable to copy file [%s] to [%s]", task.FileName, localZipFileName), err, Finished, nil)
+		return
+	}
+
+	err = sftpClient.Remove(task.FileName)
+	output <- *taskForChannel(&task, "", err, Finished, nil)
 }
+func uploadFile(task ServerTask, output chan<- ServerTask) {
+	destFilePath := path.Join("/var/tmp/", path.Base(task.UploadLocalFile))
 
-func uploadFile(serverConf *configProvider.ConfigServerType, server string, localFile string, destFolder string, output chan<- string) {
-	destFilePath := path.Join(destFolder, path.Base(localFile))
-
-	logHelper.LogPrintf("Uploading File=[%s] to server=[%s] to dest path=[%s] ...", localFile, server, destFilePath)
-
-	sshAdv := sshHelper.OpenSshAdvanced(serverConf, server)
+	sshAdv := sshHelper.OpenSshAdvanced(&task.ConfigServer, task.Server)
 	defer sshAdv.Close()
 	sftpClient := sshAdv.NewSftpClient()
 	dstFile, err := sftpClient.Create(destFilePath)
 	if err != nil {
-		logHelper.ErrFatalWithMessage(
-			fmt.Sprintf("Unable to create file=[%s] on server=[%s]", destFilePath, server),
-			err)
+		output <- *taskForChannel(&task, fmt.Sprintf("Unable to create file[%s]", destFilePath), err, Finished, nil)
+		return
 	}
 
-	srcFile, err := os.Open(localFile)
+	srcFile, err := os.Open(task.UploadLocalFile)
 	if err != nil {
-		logHelper.ErrFatal(err)
+		output <- *taskForChannel(&task, fmt.Sprintf("Unable to create file[%s]", destFilePath), err, Finished, nil)
+		return
 	}
 	defer srcFile.Close()
 
 	_, err = io.Copy(dstFile, srcFile)
-	if err != nil {
-		logHelper.ErrFatal(err)
-	}
-	output <- fmt.Sprintf("Created File: %s target server: %s", destFilePath, server)
+	output <- *taskForChannel(&task, fmt.Sprintf("Unable to create file[%s]", destFilePath), err, Finished, nil)
 }
