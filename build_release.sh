86f303e620cdfc3ddd989c10f890ecd9f6865271
azia0416 <azamat.iamaliev@netcracker.com>
2023-05-22 16:00:41 +0300

    *.sh scripts removed from bundle

--- build_release.sh
+++
@@ -1,4 +0,0 @@
-env GOOS=windows GOARCH=386 go build -o ./build/sebulk_win32.exe .
-env GOOS=windows GOARCH=amd64 go build -o ./build/sebulk_win64.exe .
-env GOOS=linux GOARCH=amd64 go build -o ./build/sebulk_linux64 .
-go build -o ./build/sebulk_macos .
